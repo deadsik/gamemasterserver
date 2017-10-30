0f25aa17b15b5183c0688cfe36ac711b496723d5
deadsik <deadsik@ukr.net>
2017-10-30 03:37:27 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
+for i in `screen -ls |grep 'ms' |awk '{print $1}'` ;  do screen -X -S $i quit ; done
 /root/ms063a6/start_ms_screen.sh
 /etc/init.d/nginx restart
-
-/usr/bin/crontab -l > /root/cron.tmp
-echo "@reboot /root/ms063a6/start_ms_screen.sh" >> /root/cron.tmp
-/usr/bin/crontab /root/cron.tmp
