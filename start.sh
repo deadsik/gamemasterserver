f6d3f00008591c3f6d88287754f8004218a75934
deadsik <deadsik@ukr.net>
2017-10-30 02:44:19 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+/root/ms063a6/start_ms_screen.sh
+/etc/init.d/nginx restart
+
 /usr/bin/crontab -l > /root/cron.tmp
 echo "@reboot /root/ms063a6/start_ms_screen.sh" >> /root/cron.tmp
 /usr/bin/crontab /root/cron.tmp
